9a709b2649d0d698f84801cdddda0d30eb3ad75f
niteshkumar257 <niteshreddy257@gmail.com>
2024-05-15 13:23:49 +0530

    variable questions

---
+++ int_string.js
@@ -0,0 +1,8 @@
+console.log(2+3+"8");
+console.log("3"+4+8);
+console.log( typeof("4"+6-8));
+console.log("34"-10);
+let a="45"-10;
+console.log(a,typeof(a));
+console.log(2-4+"45");
+console.log("45"+2-4);
